5978a1423050468cc9f10797a5849bebacc6d2ea
ask <alex@kanstul.xyz>
2022-08-21 17:31:11 -0700

    Learned `img.setScaleX(-img.getScaleX())\, which is what we were going for in the first place, and the value of taking a mid-day nap.

--- app/src/main/java/com/example/hallomoto/MainActivity.kt
+++ app/src/main/java/com/example/hallomoto/MainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import android.widget.ImageView
+<<<<<<< HEAD
 import android.view.View
+=======
+>>>>>>> 9e17e68 (Learned `img.setScaleX(-img.getScaleX())`, which is what we were going for in the first place, and the value of taking a mid-day nap.)
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val btn = findViewById<Button>(R.id.button)
         val img = findViewById<ImageView>(R.id.ImageView3)
+<<<<<<< HEAD
         val imj = findViewById<ImageView>(R.id.ImageView4)
         val vis = true;
         imj.setVisibility(View.INVISIBLE)
@@ -32,5 +36,13 @@ class MainActivity : AppCompatActivity() {
             // Emulator is laggy enough that I literally can't tell if the image isn't appearing because I did something wrong, or due to the lag.
         }
         //img.setScaleType(-img.getScaleType()
+=======
+
+        btn.setOnClickListener {
+            Toast.makeText(this, "Yes!", Toast.LENGTH_SHORT).show()
+            Log.v("Naisu","Button was pressed!")
+            img.setScaleX(-img.getScaleX())
+        }
+>>>>>>> 9e17e68 (Learned `img.setScaleX(-img.getScaleX())`, which is what we were going for in the first place, and the value of taking a mid-day nap.)
     }
 }
